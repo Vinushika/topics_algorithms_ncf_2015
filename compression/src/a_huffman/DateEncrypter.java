436f29df920da3e94eda823d44e22a2f3c3c37b7
davidhw <David.Weinstein@ncf.edu>
2015-02-11 06:31:32 -0500

    Converting ascii chars to ints in encrypter

--- compression/src/a_huffman/DateEncrypter.java
+++ compression/src/a_huffman/DateEncrypter.java
@@ -1,5 +1,6 @@
 package a_huffman;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 
 /*
@@ -18,16 +19,19 @@ public class DateEncrypter {
 	Integer currentDate = null;
 	DecompressStream inputStreamDecode = null;
 	int len;
+    boolean once = true;
 	CompressStream outputStreamEncode = null;
 
-	public DateEncrypter (DecompressStream inputStreamDecode){
+	public DateEncrypter (DecompressStream inputStreamDecode) throws IOException{
 		len = inputStreamDecode.readRecord(buffer1, 0,9);
 		if (len !=9){
 			throw new IllegalArgumentException("Error: Input DecompressStream must be pointing to file of at least 9 bytes");
 		} else{
 			this.inputStreamDecode = inputStreamDecode;
-			firstDate = ByteBuffer.wrap(buffer1).getInt();
-			inputStreamDecode.readRecord(dump,0,89);
+            firstDate = Integer.parseInt(new String(buffer1, "UTF-8"));
+
+			//firstDate = ByteBuffer.wrap(buffer1).getInt();
+			inputStreamDecode.readRecord(dump, 0, 89);
 		}
 	}
 	
@@ -35,12 +39,20 @@ public class DateEncrypter {
 		return firstDate;
 	}
 
-	public Integer getNextDate(){
-		len = inputStreamDecode.readRecord(buffer1,numEncrypted*98,9);
-		if (len != 9){
+	public Integer getNextDate() throws IOException {
+		//len = inputStreamDecode.readRecord(buffer1,numEncrypted*98,9);
+        len = inputStreamDecode.readRecord(buffer1,0,9);
+
+        if (len != 9){
 			throw new IllegalArgumentException("Error: Input DecompressStream must be pointing to file of at least 9 bytes");
 		} else{
-			currentDate = ByteBuffer.wrap(buffer1).getInt() - firstDate;
+            currentDate = Integer.parseInt(new String(buffer1, "UTF-8")) - firstDate;
+            if (once){
+             System.out.println("currentDate");
+            System.out.println(currentDate);
+            once = false;
+            }
+			//currentDate = ByteBuffer.wrap(buffer1).getInt() - firstDate;
 			inputStreamDecode.readRecord(dump, 0, 89);
 			return currentDate;
 		}
